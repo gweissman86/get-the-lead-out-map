cc7087026ab0d5b054004da92e7dc6e7f7d8f597
DanielJDufour <daniel.j.dufour@gmail.com>
2019-09-30 02:45:46 +0000

    added indenting to schools.json

--- csv-to-json.py
+++ csv-to-json.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-
-in_file = open('data/ca_schools_lead_testing_data_september_geocoded_2019_09_26_11_43_46.csv')
+infilepath = 'data/ca_schools_lead_testing_data_september_geocoded_2019_09_28_23_35_59_cleaned.csv'
+in_file = open(infilepath)
 
 reader = csv.DictReader(in_file)
 
@@ -10,14 +10,14 @@ districts = {}
 
 for line in reader:
     districtName = line['district']
-    schoolName = line['school']
-    
-    if districtName not in districts: 
+    schoolName = line['schoolName']
+
+    if districtName not in districts:
         districts[districtName] = {}
 
     districts[districtName][schoolName] = {'schoolAddress' : line['schoolAddress'], 'county' : line['county'], 'city' : line['city'], 'latitude' : line['latitude'], 'longitude' : line['longitude'], 'status' : line['status'], 'lead' : line['lead'], 'medianResult' : line['medianResult']}
 
 
-# create JSON file and write 
+# create JSON file and write
 with open("schools.json", "w") as write_file:
-    json.dump(districts, write_file)
+    json.dump(districts, write_file, indent=4)
