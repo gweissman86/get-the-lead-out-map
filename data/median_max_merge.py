90d8796351b19f19b0b17a5f7eec08e9a30da16d
DanielJDufour <daniel.j.dufour@gmail.com>
2019-10-09 03:06:26 +0000

    merge update

--- data/median_max_merge.py
+++ data/median_max_merge.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-geocoded_data = pd.read_csv('ca_schools_lead_testing_data_september_geocoded_2019_09_28_23_25_cleaned.csv')
+geocoded_data = pd.read_csv('ca_schools_lead_testing_data_september_geocoded_2019_09_28_23_35_59_cleaned.csv')
 max_results = pd.read_csv('ca_schools_lead_testing_data_with_max_results.csv', usecols=['schoolName', 'schoolAddress', 'maxResult'])
 
-median_and_max = pd.merge(geocoded_data, max_results, on=['schoolname', 'schoolAddress'], how='outer')
+median_and_max = pd.merge(geocoded_data, max_results, on=['schoolName', 'schoolAddress'], how='outer')
 
-median_and_max.to_csv('ca_schools_mean_max_lead_data.csv', index=False, compression=None)
+median_and_max.to_csv('ca_schools_median_max_lead_data.csv', index=False, compression=None)
